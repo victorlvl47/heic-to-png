78a29ea6d14bb8688e399f821fab74c9dd656f17
victor <victorlvl47@gmail.com>
2023-05-01 18:58:09 -0600

    removed comments

--- main.py
+++ main.py
@@ -25,12 +25,5 @@ for filename in os.listdir(directory):
         new_filepath = os.path.join(directory, new_filename)
 
         image.save(new_filepath, format("png"))
-        
-#         # create a new filename for the PNG file
-#         new_filename = os.path.splitext(filename)[0] + ".png"
-#         new_filepath = os.path.join(directory, new_filename)
-        
-#         # save the Image object as a PNG file
-#         image.save(new_filepath, "PNG")
 
 
